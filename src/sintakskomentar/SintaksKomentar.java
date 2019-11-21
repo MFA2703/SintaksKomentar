1a8d2835438dc6c62b9d8c5cf638fd7392ebad25
MFA2703 <56874735+MFA2703@users.noreply.github.com>
2019-11-22 01:09:45 +0700

    Add files via upload

---
+++ src/sintakskomentar/SintaksKomentar.java
@@ -0,0 +1,14 @@
+package sintakskomentar;
+
+//setiap kode yang berjalan di java harus berada dalam class (ini adalah komentar)
+//setiap class haru berawalkan huruf besar
+//nama file java harus sama dengan nama class
+public class SintaksKomentar {
+
+    //main method diperlukan untuk mengeksekusi setiap kode java didalamnya
+    public static void main(String[] args) {
+        //didalam main method bisa menggunakan println methode untuk mencetak satu baris teks ke layar
+        System.out.println("Hello World My Name Is Muhammad Fazron Arif");
+    }
+    
+}
